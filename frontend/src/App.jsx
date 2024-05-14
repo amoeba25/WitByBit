e903064abeb0a50fb5ed1860dcac318b64e3e7c7
amoeba25 <23sohamsh@gmail.com>
2024-05-14 07:27:35 +0530

    basic table setup and cors handling

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { useMemo } from "react";
+import "./App.css";
+import fakeData from "./fakeData.json";
+import { useTable } from "react-table";
+import Contact from "./components/Contact";
+
+function App() {
+  return <Contact />;
+}
+
+export default App;
